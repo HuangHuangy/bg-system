d5d17e5b5725ac45549df1a07435f61d029f1d84
HuangHuangy <18202531413@163.com>
2018-11-27 09:56:31 +0800

    将store分割成app和user两个模块

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,19 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import user from './modules/user'
+import app from './modules/app'
 
 Vue.use(Vuex)
+
+export default new Vuex.Store({
+  state: {
+  },
+  mutations: {
+  },
+  actions: {
+  },
+  modules: {
+    user,
+    app
+  }
+})
